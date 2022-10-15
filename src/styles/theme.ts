4aafb64a4659ba0895bbae4b1fac86d8a05aff49
Iev Khov <ievkhov1@gmail.com>
2022-10-15 16:22:53 -0700

    Install MUI

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { ThemeOptions } from "@mui/material/styles";
+
+const defaultTheme: ThemeOptions = {
+  shape: {
+    borderRadius: 8,
+  },
+  typography: {
+    fontFamily: "Montserrat, sans-serif",
+  },
+};
+
+export default defaultTheme;
